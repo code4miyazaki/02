9c452f21572e98db1baa2e4b0a62890cd7ae2a3c
code4miyazaki <48007293+code4miyazaki@users.noreply.github.com>
2019-03-02 16:57:23 +0900

    Add files via upload

---
+++ subscriber.php
@@ -0,0 +1,38 @@
+<?php
+include 'ChromePhp.php';
+ChromePhp::log('Hello console!日本語もOK');
+$USER= 'kenzkenz_miya';
+$PW= 'miyazaki';
+$dnsinfo= "mysql:dbname=kenzkenz_kisyou;host=mysql1203b.xserver.jp;charset=utf8";
+$pdo = new PDO($dnsinfo,$USER,$PW);
+
+if( isset( $_GET[ 'hub_challenge' ] )  )  {
+    header( 'HTTP/1.1 200' );
+    header( 'Content-Type: text/plain' );
+    echo $_GET[ 'hub_challenge' ];
+    file_put_contents( 'challenge.txt', print_r( $_GET, TRUE ) );
+} else  {
+    $string = file_get_contents( 'php://input' );
+    file_put_contents( 'new-feed.txt', $string );
+    $feed = simplexml_load_string($string);
+
+    $stmt = $pdo -> prepare("INSERT INTO tblfeed (id, title, updated, author, link, xml) VALUES (:id, :title, :updated, :author, :link, :xml)");
+    $stmt->bindParam(':id', $id, PDO::PARAM_STR);
+    $stmt->bindParam(':title', $title, PDO::PARAM_STR);
+    $stmt->bindParam(':updated', $updated, PDO::PARAM_STR);
+    $stmt->bindParam(':author', $author, PDO::PARAM_STR);
+    $stmt->bindParam(':link', $link, PDO::PARAM_STR);
+    $stmt->bindParam(':xml', $xml, PDO::PARAM_STR);
+
+    foreach ($feed->entry as $entry) {
+        $id = $entry->id;
+        $title= $entry->title;
+        $updated= $entry->updated;
+        $author= $entry->author->name;
+        $link = $entry->link['href'];
+    }
+    $xml = $string;
+    $stmt->execute();
+
+}
+
